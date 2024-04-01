a7ff2fcbc0cc8013c17874f4cb402cb8c4060399
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-04-01 13:32:02 +0000

    Dreytechstore

---
+++ Home/payment.tsx
@@ -0,0 +1,239 @@
+import React from 'react';
+
+export default function Payment() {
+    return (
+      <div className="flex flex-col items-center w-[1366px] box-sizing-border">
+        <img className="absolute left-[0px] top-[0px] right-[0px] bottom-[0px] h-[100%]" />
+        <div className="relative m-[0_0_23px_0] flex flex-col items-center p-[0_0_65.9px_0] w-[1366px] box-sizing-border">
+          <img className="absolute left-[0px] top-[0px] right-[0px] bottom-[0px] h-[100%]" />
+          <div className="relative m-[0_0_54px_0] flex flex-col items-end p-[13px_4px_0_0] w-[1366px] box-sizing-border">
+            <img className="absolute left-[0px] top-[0px] right-[0px] bottom-[0px] h-[100%]" />
+            <div className="relative m-[0_52.2px_11px_52.2px] flex flex-row justify-between w-[256.5px] box-sizing-border">
+              <span className="m-[0_7.5px_0_0] w-[77px] break-words font-['Montserrat'] font-normal text-[12px] text-[#4A4B4D]">
+                Return
+              </span>
+              <span className="break-words font-['Montserrat'] font-normal text-[12px] text-[#4A4B4D]">
+                Help
+              </span>
+              <span className="break-words font-['Montserrat'] font-normal text-[12px] text-[#4A4B4D]">
+                Register / Sign In
+              </span>
+            </div>
+            <img className="relative w-[1362px] h-[1px]" />
+          </div>
+          <div className="relative m-[0_49px_0_49px] flex flex-row justify-between w-[1268px] box-sizing-border">
+            <div className="m-[12px_0_0.1px_0] flex flex-row justify-center w-[22px] h-[22px] box-sizing-border">
+              <img className="w-[22px] h-[22px]" />
+            </div>
+            <div className="relative m-[13px_0_1.1px_0] flex flex-row justify-between w-[786.3px] h-[fit-content] box-sizing-border">
+              <div className="flex flex-row justify-center box-sizing-border">
+                <span className="break-words font-['Montserrat'] font-normal text-[16px] text-[#222222]">
+                  SHOP
+                </span>
+              </div>
+              <div className="flex flex-row justify-center box-sizing-border">
+                <span className="break-words font-['Montserrat'] font-normal text-[16px] text-[#222222]">
+                  ESSENTIALS
+                </span>
+              </div>
+              <div className="flex flex-row justify-center box-sizing-border">
+                <span className="break-words font-['Montserrat'] font-normal text-[16px] text-[#222222]">
+                  BEST SELLERS
+                </span>
+              </div>
+              <div className="flex flex-row justify-center box-sizing-border">
+                <span className="break-words font-['Montserrat'] font-normal text-[16px] text-[#222222]">
+                  ABOUT US
+                </span>
+              </div>
+            </div>
+            <div className="flex flex-row justify-between w-[132px] box-sizing-border">
+              <div className="m-[12.1px_0_0_0] flex flex-row justify-center w-[18.7px] h-[22px] box-sizing-border">
+                <img className="w-[18.7px] h-[22px]" />
+              </div>
+              <div className="m-[0_0_0px_0] flex flex-row w-[76px] box-sizing-border">
+                <div className="m-[0_29px_0_0] flex flex-col items-center w-[28px] box-sizing-border">
+                  <div className="m-[0_0_0.1px_16px] flex flex-row justify-center w-[12px] h-[12px] box-sizing-border">
+                    <img className="w-[12px] h-[12px]" />
+                  </div>
+                  <img className="m-[0_8.2px_4.4px_0] w-[19.8px] h-[16.5px]" />
+                  <img className="m-[0_16.1px_0_8px] w-[3.8px] h-[1.1px]" />
+                </div>
+                <img className="m-[12.2px_0_0px_0] w-[19px] h-[21.9px]" />
+              </div>
+            </div>
+          </div>
+          <p className="absolute left-[50%] bottom-[24px] translate-x-[-50%] text-center break-words font-['Monotype_Corsiva','Roboto_Condensed'] font-normal text-[40px] text-[#E2342D]">
+            <span className="macc-essentials-sub-5"></span><span></span>
+          </p>
+        </div>
+        <div className="relative flex flex-col items-center w-[fit-content] box-sizing-border">
+          <div className="relative m-[0_49px_26px_49px] flex flex-row p-[0_0_1px_0] w-[fit-content] box-sizing-border">
+            <img className="absolute left-[0px] top-[0px] w-[1264px] h-[597px]" />
+            <div className="relative m-[2px_57.8px_0_0] flex flex-col w-[613px] box-sizing-border">
+              <div className="m-[0_1px_20.7px_1px] flex flex-row justify-center self-start box-sizing-border">
+                <span className="break-words font-['Poppins'] font-bold text-[36px] text-[#222222]">
+                  PAYMENT
+                </span>
+              </div>
+              <div className="relative m-[0_5px_26.7px_1px] flex flex-row justify-center p-[0_0_147px_0] w-[607px] box-sizing-border">
+                <div className="relative flex flex-row p-[12px_0_15px_12px] w-[607px] box-sizing-border">
+                  <img className="absolute left-[0px] top-[0px] right-[0px] bottom-[0px] h-[100%]" />
+                  <img className="relative m-[5px_20px_0_0] w-[18px] h-[18px]" />
+                  <div className="relative m-[0_0_1px_0] flex flex-row justify-center box-sizing-border">
+                    <span className="break-words font-['Montserrat'] font-medium text-[18px] text-[#222222]">
+                      Bitcoin
+                    </span>
+                  </div>
+                </div>
+                <div className="absolute left-[50%] top-[49px] translate-x-[-50%] flex flex-row p-[12px_0_15px_12px] w-[607px] box-sizing-border">
+                  <img className="absolute left-[0px] top-[0px] right-[0px] bottom-[0px] h-[100%]" />
+                  <img className="relative m-[5px_20px_0_0] w-[18px] h-[18px]" />
+                  <div className="relative m-[0_0_1px_0] flex flex-row justify-center box-sizing-border">
+                    <span className="break-words font-['Montserrat'] font-medium text-[18px] text-[#222222]">
+                      Apple Wallet
+                    </span>
+                  </div>
+                </div>
+                <div className="absolute left-[50%] bottom-[49px] translate-x-[-50%] flex flex-row p-[12px_0_15px_12px] w-[607px] box-sizing-border">
+                  <img className="absolute left-[0px] top-[0px] right-[0px] bottom-[0px] h-[100%]" />
+                  <img className="relative m-[5px_20px_0_0] w-[18px] h-[18px]" />
+                  <div className="relative m-[0_0_1px_0] flex flex-row justify-center box-sizing-border">
+                    <span className="break-words font-['Montserrat'] font-medium text-[18px] text-[#222222]">
+                      Paypal
+                    </span>
+                  </div>
+                </div>
+                <div className="absolute left-[50%] bottom-[0px] translate-x-[-50%] flex flex-row p-[12px_0_15px_12px] w-[607px] box-sizing-border">
+                  <img className="absolute left-[0px] top-[0px] right-[0px] bottom-[0px] h-[100%]" />
+                  <img className="relative m-[5px_20px_0_0] w-[18px] h-[18px]" />
+                  <div className="relative m-[0_0_1px_0] flex flex-row justify-center box-sizing-border">
+                    <span className="break-words font-['Montserrat'] font-medium text-[18px] text-[#222222]">
+                      Debit / Credit Card
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div className="m-[0_1px_25.6px_0] flex flex-col w-[fit-content] box-sizing-border">
+                <div className="m-[0_0_22px_0] inline-block self-start break-words font-['Poppins'] font-bold text-[20px] text-[#222222]">
+                  Delivery Address
+                </div>
+                <div className="flex flex-col w-[fit-content] box-sizing-border">
+                  <div className="relative m-[0_0_19px_0] p-[12px_20px_11px_20px] w-[fit-content] box-sizing-border">
+                    <img className="absolute left-[0px] top-[0px] right-[0px] bottom-[0px] h-[100%]" />
+                    <span className="relative break-words font-['Montserrat'] font-normal text-[18px] text-[#222222]">
+                      4568 8465 1258 6895
+                    </span>
+                  </div>
+                  <div className="relative m-[0_0_19px_0] p-[12px_20px_11px_20px] w-[fit-content] box-sizing-border">
+                    <img className="absolute left-[0px] top-[0px] right-[0px] bottom-[0px] h-[100%]" />
+                    <span className="relative break-words font-['Montserrat'] font-normal text-[18px] text-[#222222]">
+                      John Wick
+                    </span>
+                  </div>
+                  <div className="flex flex-row w-[fit-content] box-sizing-border">
+                    <div className="relative m-[0_22px_0_0] p-[12px_20px_11px_20px] w-[295px] box-sizing-border">
+                      <img className="absolute left-[0px] top-[0px] right-[0px] bottom-[0px] h-[100%]" />
+                      <span className="relative break-words font-['Montserrat'] font-normal text-[18px] text-[#222222]">
+                        12/24
+                      </span>
+                    </div>
+                    <div className="relative p-[12px_20px_11px_20px] w-[295px] box-sizing-border">
+                      <img className="absolute left-[0px] top-[0px] right-[0px] bottom-[0px] h-[100%]" />
+                      <span className="relative break-words font-['Montserrat'] font-normal text-[18px] text-[#222222]">
+                        584
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="m-[0_0_0_1px] flex flex-row justify-between w-[612px] box-sizing-border">
+                <div className="m-[14px_0_13.5px_0] flex flex-row box-sizing-border">
+                  <div className="m-[2.6px_16.2px_0_0] flex flex-row justify-center w-[8.1px] h-[15px] box-sizing-border">
+                    <img className="w-[8.1px] h-[15px]" />
+                  </div>
+                  <div className="m-[0_0_0.5px_0] inline-block break-words font-['Montserrat'] font-normal text-[14px] text-[#231F20]">
+                    Go back to checkout
+                  </div>
+                </div>
+                <div className="flex flex-row justify-center w-[258px] box-sizing-border">
+                  <div className="shadow-[0px_3px_6px_0px_rgba(0,0,0,0.051)] relative flex flex-row justify-center p-[7px_0_2px_1.6px] w-[258px] h-[fit-content] box-sizing-border">
+                    <img className="absolute left-[50%] top-[0px] translate-x-[-50%] w-[258px] h-[45px]" />
+                    <span className="relative break-words font-['Poppins'] font-bold text-[24px] text-[#FFFFFF]">
+                      Pay ₹ 40
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="relative m-[0_0_0.2px_0] flex flex-row justify-center w-[597.3px] h-[595.8px] box-sizing-border">
+              <img className="w-[597.3px] h-[595.8px]" />
+            </div>
+          </div>
+          <div className="relative flex flex-col items-center p-[29.9px_52.8px_36px_97.6px] w-[fit-content] box-sizing-border">
+            <img className="absolute left-[0px] top-[0px] right-[0px] bottom-[0px] h-[100%]" />
+            <div className="relative m-[0_0_73.5px_0] flex flex-row w-[fit-content] box-sizing-border">
+              <div className="m-[0_177.2px_96.6px_0] flex flex-row justify-between w-[607.2px] h-[fit-content] box-sizing-border">
+                <div className="flex flex-row box-sizing-border">
+                  <p className="m-[0_39.6px_0_0] break-words font-['Monotype_Corsiva','Roboto_Condensed'] font-normal text-[54px] text-[#E2342D]">
+                    <span className="macc-essentials-1-sub-5"></span><span></span>
+                  </p>
+                  <div className="m-[8px_0_16px_0] flex flex-col box-sizing-border">
+                    <div className="m-[0_0_21px_0] flex flex-row justify-center self-start box-sizing-border">
+                      <span className="break-words font-['Montserrat'] font-normal text-[18px] text-[#222222]">
+                        Home
+                      </span>
+                    </div>
+                    <div className="m-[0_0_21px_0] flex flex-row justify-center box-sizing-border">
+                      <span className="break-words font-['Montserrat'] font-normal text-[18px] text-[#222222]">
+                        Collection
+                      </span>
+                    </div>
+                    <div className="m-[0_8.8px_0_0] flex flex-row justify-center box-sizing-border">
+                      <span className="break-words font-['Montserrat'] font-normal text-[18px] text-[#222222]">
+                        Products
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <div className="m-[9.1px_0_15.9px_0] flex flex-col box-sizing-border">
+                  <div className="m-[0_0_21px_0] inline-block break-words font-['Montserrat'] font-normal text-[18px] text-[#222222]">
+                    About
+                  </div>
+                  <div className="m-[0_0.3px_22px_0] inline-block break-words font-['Gotham','Roboto_Condensed'] font-normal text-[18px] text-[#222222]">
+                    Contact
+                  </div>
+                  <span className="self-start break-words font-['Gotham','Roboto_Condensed'] font-normal text-[18px] text-[#222222]">
+                    FAQ
+                  </span>
+                </div>
+              </div>
+              <div className="m-[10.1px_0_0_0] flex flex-col box-sizing-border">
+                <div className="m-[0_0_34px_0] inline-block break-words font-['Montserrat'] font-normal text-[18px] leading-[1.4] text-[#222222]">
+                  Be the first to know about our biggest and best sales. We&#39;ll never send more than one email a month.
+                </div>
+                <div className="relative m-[0_30.7px_21px_0] flex flex-row justify-center p-[0_0.5px_0_0] box-sizing-border">
+                  <div className="bg-[#FFFFFF] relative flex flex-col p-[14px_0_0_0] box-sizing-border">
+                    <div className="m-[0_0_13px_0] inline-block self-start break-words font-['Montserrat'] font-normal text-[18px] text-[#222222]">
+                      ENTER YOUR EMAIL
+                    </div>
+                    <img className="w-[400px] h-[1px]" />
+                  </div>
+                  <img className="absolute right-[0px] bottom-[14px] w-[27.5px] h-[22px]" />
+                </div>
+                <div className="flex flex-row self-start gap-[0_17px] w-[203px] box-sizing-border">
+                  <img className="m-[0.5px_0_0_0] w-[38px] h-[38px]" />
+                  <img className="m-[0.5px_0_0_0] w-[38px] h-[38px]" />
+                  <img className="m-[0_0_0.5px_0] w-[38px] h-[38px]" />
+                  <img className="m-[0_0_0.5px_0] w-[38px] h-[38px]" />
+                </div>
+              </div>
+            </div>
+            <span className="relative m-[0_44.8px_0_0] break-words font-['Montserrat'] font-normal text-[18px] text-[#222222]">
+              All rights are reserved
+            </span>
+          </div>
+        </div>
+      </div>
+    )
+  }
